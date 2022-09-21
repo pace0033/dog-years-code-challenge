de03d5054bded53f37665167fd47465cd17fd93e
Will Paceley <pace0033@algonquinlive.com>
2022-09-21 09:49:35 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/* --- JS CODE CHALLENGE --- */
+// Create a function to convert dog years to human years
+// The function will take one parameter, dogAge
+// The function will return a number that is the human year equivalent of dog years
+// If the user supplies an invalid age, return a string with an error message
+// The first year of a dog's life is equal to 15 human years
+// The second year of a dog's life is equal to 9 human years
+// After that, every human year equals approximately five years for a dog
+
+/* --- YOUR CODE HERE --- */
+
+/* --- END YOUR CODE --- */
+
+/* --- TESTS --- */
+
+console.log(dogToHumanYears(4)); // Expect: 34
+console.log(dogToHumanYears(10)); // Expect: 64
+console.log(dogToHumanYears(1)); // Expect: 15
+console.log(dogToHumanYears(2)); // Expect: 24
+console.log(dogToHumanYears(-42)); // Expect: error message
